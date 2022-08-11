837a30a894774f9fad8f65becd8e249240d4caf3
galenomoon <gui.galenocover@gmail.com>
2022-08-11 13:55:52 -0300

    Modal Organization

--- App.js
+++ App.js
@@ -1,12 +1,11 @@
-import React, { useState, useEffect } from 'react';
-import { View, Text } from 'react-native';
-import { Camera, CameraType } from 'expo-camera';
+import React from 'react';
+import { View } from 'react-native';
 import tw from 'twrnc';
 import ModalConfirmation from './components/ModalConfirmation';
 
 export default function App() {
   return (
-    <View style={tw`flex flex-1 bg-purple-400 items-center justify-center`}>
+    <View style={tw`flex flex-1 bg-[#333] items-center justify-center`}>
       <ModalConfirmation/>
     </View>
   );
--- components/ModalConfirmation/index.jsx
+++ components/ModalConfirmation/index.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, TouchableOpacity, Modal } from 'react-native';
+import { View, Text, TouchableOpacity as TO, Modal } from 'react-native';
 import { Camera, CameraType } from 'expo-camera';
 import tw from 'twrnc';
 
@@ -14,25 +14,22 @@ export default function ModalConfirmation() {
     })();
   }, []);
 
-  if (hasPermission === null) {
-    return <View />;
-  }
-  if (hasPermission === false) {
-    return <Text>No access to camera</Text>;
-  }
+  if (hasPermission === null) return <View />;
+  if (hasPermission === false) return <Text>No access to camera</Text>
+  
   return (
-    <Modal style={tw`flex flex-col justify-center items-center`} visible={true}>
-      <View style={tw`flex bg-purple-400`}>
-        <View style={tw`flex bg-white w-[320px] h-[400px] items-center p-[30px] rounded-2`}>
-          <View style={tw`flex w-[300px] h-[300px] overflow-hidden`}>
-            <Camera style={tw`w-[100%] h-[100] bg-gray-300`} type={type} />
+    <Modal animationType="slide" transparent={true} visible={true}>
+      <View style={tw`w-full h-full bg-black opacity-30 absolute`} />
+      <View style={tw`flex-1 justify-center items-center`}>
+        <View style={tw`flex items-center justify-center bg-[#333] w-[90%] h-[70%] rounded-2xl`}>
+          <View style={tw`flex bg-white w-[320px] h-[400px] items-center p-[30px] rounded-2`}>
+            <View style={tw`flex w-[300px] h-[300px] overflow-hidden`}>
+              <Camera style={tw`w-[100%] h-[100] bg-gray-300`} type={type} />
+            </View>
+            <TO onPress={() => setType(type === CameraType.back ? CameraType.front : CameraType.back)}>
+              <Text> FLIP </Text>
+            </TO>
           </View>
-          <TouchableOpacity
-            onPress={() => {
-              setType(type === CameraType.back ? CameraType.front : CameraType.back);
-            }}>
-            <Text> FLIP </Text>
-          </TouchableOpacity>
         </View>
       </View>
     </Modal>
