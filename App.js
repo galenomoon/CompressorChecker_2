cbd247d69dff10fd2f8e1ec0371478bac8899509
galenomoon <gui.galenocover@gmail.com>
2022-08-12 13:32:15 -0300

    Gradient applied

--- App.js
+++ App.js
@@ -1,29 +1,30 @@
 import React, { useState } from 'react';
 import { View, Switch, Text, Image } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
 import tw from 'twrnc';
 import ModalConfirmation from './components/ModalConfirmation';
-import { useFonts, Inter_900Black, Inter_800ExtraBold } from '@expo-google-fonts/inter';
 
 export default function App() {
   const [toggle, setToggle] = useState(true)
   const [showModal, setShowModal] = useState(false)
-  let [fontsLoaded] = useFonts({ Inter_900Black, Inter_800ExtraBold });
 
   return (
-    <View style={tw`flex flex-1 ${toggle ? 'bg-[#e6252e]' : 'bg-[#0ad3f2]'} items-center justify-center`}>
-      <Image style={{ transform: [{ scale: 0.8 }] }} source={toggle ? require('./assets/grinning-face.png') : require('./assets/very-happy-face.png')} />
-      <View style={tw`flex items-center justify-center bg-white rounded-3xl shadow-lg p-3`}>
-        <Text style={[tw`text-2xl `, { fontFamily: 'Inter_900' }]}>O COMPRESSOR ESTÁ</Text>
-        <Text style={[tw`text-5xl `, { fontFamily: 'Inter_800ExtraBold' }]}>{toggle ? 'LIGADO!' : 'DESLIGADO'}</Text>
-        <Switch
-          value={toggle}
-          onValueChange={() => [setShowModal(true)]}
-          thumbColor={toggle ? '#777' : "#777"}
-          trackColor={{ true: "#c63039", false: "#bbb" }}
-          style={{ transform: [{ scaleX: 2 }, { scaleY: 2 }] }}
-        />
-      </View>
-      <ModalConfirmation show={showModal} toggle={toggle} turnOffCompressor={() => setToggle(false)} turnOnCompressor={() => setToggle(true)} close={() => setShowModal(!showModal)} />
+    <View style={tw`flex flex-1  items-center justify-center`}>
+      <LinearGradient style={tw`flex flex-1 justify-center items-center`} colors={toggle ? ['#cb0000', '#d6ac25', '#cb0000'] : ['#C13584', '#E1306C', '#FCAF45', '#F2f']} >
+        <Image style={{ transform: [{ scale: 0.8 }] }} source={toggle ? require('./assets/grinning-face.png') : require('./assets/very-happy-face.png')} />
+        <View style={tw`flex items-center justify-center bg-white rounded-3xl shadow-lg p-3`}>
+          <Text style={[tw`text-2xl font-bold`]}>O COMPRESSOR ESTÁ</Text>
+          <Text style={[tw`text-5xl font-bold`]}>{toggle ? 'LIGADO!' : 'DESLIGADO'}</Text>
+          <Switch
+            value={toggle}
+            onValueChange={() => [setShowModal(true)]}
+            thumbColor={toggle ? '#777' : "#777"}
+            trackColor={{ true: "#c63039", false: "#bbb" }}
+            style={{ transform: [{ scaleX: 2 }, { scaleY: 2 }] }}
+          />
+        </View>
+        <ModalConfirmation show={showModal} toggle={toggle} turnOffCompressor={() => setToggle(false)} turnOnCompressor={() => setToggle(true)} close={() => setShowModal(!showModal)} />
+      </LinearGradient>
     </View>
   );
 }
--- components/ModalConfirmation/index.jsx
+++ components/ModalConfirmation/index.jsx
@@ -1,16 +1,14 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, TouchableOpacity as TO, Modal } from 'react-native';
+import { View, Text, TouchableOpacity as TO, Modal, Image } from 'react-native';
 import { Camera, CameraType } from 'expo-camera';
 import Ionicons from "react-native-vector-icons/Ionicons";
 import MaterialIcons from "react-native-vector-icons/MaterialIcons";
-import { useFonts, Inter_900Black, Inter_800ExtraBold, Inter_600SemiBold } from '@expo-google-fonts/inter';
 
 import tw from 'twrnc';
 
 export default function ModalConfirmation({ show, close, turnOffCompressor, turnOnCompressor, toggle }) {
   const [hasPermission, setHasPermission] = useState(null);
   const [type, setType] = useState(CameraType.back);
-  let [fontsLoaded] = useFonts({ Inter_900Black, Inter_800ExtraBold, Inter_600SemiBold });
 
   useEffect(() => {
     (async () => {
@@ -26,8 +24,8 @@ export default function ModalConfirmation({ show, close, turnOffCompressor, turn
     <Modal animationType="slide" transparent={true} visible={show}>
       <View style={tw`w-full h-full bg-black opacity-30 absolute`} />
       <View style={tw`flex-1 justify-center items-center`}>
-        <View style={tw`flex items-center bg-[#333] w-[90%] ${toggle ? 'h-[70%]' : 'h-[30%]'} overflow-hidden rounded-2xl shadow-xl`}>
-          <View style={tw`bg-[#555] flex  ${toggle ? 'flex-2' : 'flex-5'}  flex-row justify-end items-center w-full`}>
+        <View style={tw`flex items-center bg-[#333] w-[90%] ${toggle ? 'h-[70%]' : 'h-[50%]'} overflow-hidden rounded-2xl shadow-xl`}>
+          <View style={tw`bg-[#555] flex  ${toggle ? 'flex-2' : 'flex-2.5'}  flex-row justify-end items-center w-full`}>
             <TO onPress={close}>
               <Ionicons name='close' size={40} style={tw`mx-2 text-white rounded-xl bg-[#444] px-[1px]`} />
             </TO>
@@ -48,18 +46,19 @@ export default function ModalConfirmation({ show, close, turnOffCompressor, turn
                 </View>
               </View>
               :
-              <View style={tw`w-full h-full justify-center items-center`}>
-                <Text style={[tw`text-2xl text-center text-white `, { fontFamily: 'Inter_800ExtraBold' }]}>
+              <View style={tw`flex w-full mb-5 justify-center items-center`}>
+                <Image style={tw`w-[50%] h-[50%]`} source={require('../../assets/caution-sign.png')} />
+                <Text style={[tw`text-2xl text-center font-bold text-white mb-3`]}>
                   Deseja realmente ligar o compressor?
                 </Text>
                 <View style={tw`flex flex-row w-full items-center justify-evenly px-5`}>
-                  <TO onPress={()=> close()} style={tw`text-white flex items-center justify-center px-5 py-2 my-3 rounded-2xl`}>
-                    <Text style={[tw`text-[#777] text-lg`, { fontFamily: 'Inter_600SemiBold' }]}>
+                  <TO onPress={() => close()} style={tw`text-white flex items-center justify-center px-5 py-2 my-3 rounded-2xl`}>
+                    <Text style={[tw`text-[#777] text-lg font-bold`]}>
                       cancelar
                     </Text>
                   </TO>
-                  <TO onPress={()=> [turnOnCompressor(), close()]} style={tw`text-white flex items-center justify-center px-5 py-2 my-3 rounded-2xl bg-[#D42B15]`}>
-                    <Text style={[tw`text-white text-lg`, { fontFamily: 'Inter_600SemiBold' }]}>
+                  <TO onPress={() => [turnOnCompressor(), close()]} style={tw`text-white flex items-center justify-center px-5 py-2 my-3 rounded-2xl bg-[#D42B15]`}>
+                    <Text style={[tw`text-white text-lg font-bold`]}>
                       Confirmar
                     </Text>
                   </TO>
